8d4595d62dbc9432860d2c8e7bc6ed0d510816f7
Bakerling25 <thom97m6@edu.sde.dk>
2020-02-10 10:43:30 +0100

    Start

---
+++ SmallBankOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallBankOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
