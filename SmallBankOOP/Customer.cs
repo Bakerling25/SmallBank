b31983d52aa73073f068ad63fdefafc09dbe12a8
Bakerling25 <thom97m6@edu.sde.dk>
2020-02-10 12:22:53 +0100

    Har lavet customer klassen

--- SmallBankOOP/Customer.cs
+++ SmallBankOOP/Customer.cs
@@ -7,5 +7,38 @@ namespace SmallBankOOP
 {
     class Customer:User
     {
+        private string cpr;
+        private string fuldeNavn;
+        private string efterNavn;
+        private string addresse;
+        private string userName;
+        private List<Account> accounts;
+
+        public string Cpr
+        {
+            get { return cpr; }
+            set { cpr = value; }
+        }
+        public string FuldeNavn
+        { 
+            get { return fuldeNavn; }
+            set { fuldeNavn = value; }
+        }
+        public string EfterNavn
+        {
+            get { return efterNavn; }
+            set { efterNavn = value; }
+        }
+        public string Addresse
+        {
+            get { return addresse; }
+            set { addresse = value; }
+        }
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
     }
 }
