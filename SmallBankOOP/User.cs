dce8eda7666385a72905c36241e5221622d0a1b4
Claus Nordahl Dimon <53813074+clausdimon@users.noreply.github.com>
2020-02-10 12:20:30 +0100

    user Done first view

--- SmallBankOOP/User.cs
+++ SmallBankOOP/User.cs
@@ -6,5 +6,24 @@ namespace SmallBankOOP
 {
     abstract class User
     {
+        private int userID;
+        private string password;
+        private bool loginStatus;
+        private string email;
+        private DateTime registerDate;
+
+        public bool VerifyLogin(int id, string passwd,List<User> bruger)
+        {
+            foreach (User item in bruger)
+            {
+                if (item.userID == id && item.password == passwd)
+                {
+                    item.loginStatus = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
